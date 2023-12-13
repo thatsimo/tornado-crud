eb2b8a1c4b9235eb8e507b0ed4106a5dc66a01d4
Simone Squillace <simonewe99@gmail.com>
2023-12-13 02:15:11 +0100

    feat: add test with CRUD operations

--- test/student_test.py
+++ test/student_test.py
@@ -3,24 +3,13 @@ from tornado.testing import AsyncHTTPTestCase, gen_test
 import tornado.escape
 from app import app
 
-class TestStudentHandler(AsyncHTTPTestCase):
+class StudentTestCase(AsyncHTTPTestCase):
     def get_app(self):
         return app
 
     @gen_test
-    def test_get_students(self):
-        response = yield self.http_client.fetch(self.get_url('/students'))
-        self.assertEqual(response.code, 200)
-
-    @gen_test
-    def test_get_student_by_id(self):
-        # Assuming you have a student ID in your database
-        student_id = "6578a99fe5b7172f828ac94c"
-        response = yield self.http_client.fetch(self.get_url(f'/students/{student_id}'))
-        self.assertEqual(response.code, 200)
-
-    @gen_test
-    def test_post_student(self):
+    def test_crud_students(self):
+        # Creating a student POST /students, expect to get the created student back
         student_data ={"name": "Test Student", "age": 20} 
         response = yield self.http_client.fetch(
             self.get_url('/students'),
@@ -31,12 +20,23 @@ class TestStudentHandler(AsyncHTTPTestCase):
         res = tornado.escape.json_decode(response.body)
         self.assertEqual(res["name"], student_data["name"])
         self.assertEqual(res["age"], student_data["age"])
+        self.assertTrue(res["_id"] != "")
         self.assertTrue(res["fact"] != "")
 
-    @gen_test
-    def test_put_student(self):
-        # Assuming you have a student ID in your database
-        student_id = "6578a99fe5b7172f828ac94c"
+        student_id = res["_id"]
+
+        # Getting student by _id GET /students/{_id}, expect to get the created student back
+        response = yield self.http_client.fetch(self.get_url(f'/students/{student_id}'))
+        self.assertEqual(response.code, 200)
+
+        # Getting all students GET /students, expect to get a list of students and the created student in the list
+        response = yield self.http_client.fetch(self.get_url('/students'))
+        self.assertEqual(response.code, 200)
+        res = tornado.escape.json_decode(response.body)
+        filteredRes = list(filter(lambda student: student["_id"] == student_id, res["students"]))
+        self.assertTrue(len(filteredRes) > 0)
+
+        # Updating student PUT /students/{_id}, expect to get the updated student back
         updated_data = {"name": "Updated Student", "age": 21}
         response = yield self.http_client.fetch(
             self.get_url(f'/students/{student_id}'),
@@ -44,14 +44,19 @@ class TestStudentHandler(AsyncHTTPTestCase):
             body=tornado.escape.json_encode(updated_data)
         )
         self.assertEqual(response.code, 200)
-        self.assertEqual(response.body["name"], updated_data["name"])
+        res = tornado.escape.json_decode(response.body)
+        self.assertEqual(res["name"], updated_data["name"])
 
-    @gen_test
-    async def test_delete_student(self):
-        # Assuming you have a student ID in your database
-        student_id = "6578a99fe5b7172f828ac94c"
+        # Deleting student DELETE /students/{_id}, expect to get a 204 response
         response = yield self.http_client.fetch(self.get_url(f'/students/{student_id}'), method='DELETE')
         self.assertEqual(response.code, 204)
+
+        # Getting all students GET /students, expect to get a list of students without the created student in the list
+        response = yield self.http_client.fetch(self.get_url('/students'))
+        self.assertEqual(response.code, 200)
+        res = tornado.escape.json_decode(response.body)
+        filteredRes = list(filter(lambda student: student["_id"] == student_id, res["students"]))
+        self.assertTrue(len(filteredRes) == 0)
 
 if __name__ == '__main__':
     unittest.main()
